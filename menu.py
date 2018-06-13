ad4a00920bd2ce4482bf1333df64d74272e713f3
superpatel101 <37316661+superpatel101@users.noreply.github.com>
2018-06-12 23:08:25 -0400

    Look at checking_level1

--- menu.py
+++ menu.py
@@ -1,10 +1,11 @@
 from pygame import *
 screen=display.set_mode((800,600))
 menu_background=image.load('menu_background.png')
+door=image.load('door.png')
 sprint=[]
 frame=0
 for i in range(2,26):
-    sprint.append(image.load(str(i+1)+'.png'))
+    sprint.append(transform.scale(image.load(str(i+1)+'.png'),(50,70)))
 def menu():
     global frame
     running = True
@@ -19,8 +20,9 @@ def menu():
         mb = mouse.get_pressed()
         
         screen.blit(menu_background,(0,0))
-        screen.blit(sprint[int(frame)%22],(400,300))
-        frame+=0.3
+        screen.blit(door,(750,455))
+        screen.blit(sprint[int(frame)%22],(400,500))
+        frame+=0.6
         
 
                 
