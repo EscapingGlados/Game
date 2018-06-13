991ce6147cd5a5dfd1dff0a784a43eabad195dff
superpatel101 <37316661+superpatel101@users.noreply.github.com>
2018-06-13 02:47:02 -0400

    menu

--- menu.py
+++ menu.py
@@ -2,15 +2,23 @@ from pygame import *
 screen=display.set_mode((800,600))
 menu_background=image.load('menu_background.png')
 door=image.load('door.png')
+glados=transform.scale(image.load('glados.png'),(248,362))
 sprint=[]
+button=image.load('button.png')
+click_button=image.load('button_on_hover.png')
 frame=0
+motion=0
+switch=True
 for i in range(2,26):
-    sprint.append(transform.scale(image.load(str(i+1)+'.png'),(50,70)))
+    sprint.append(transform.smoothscale(image.load(str(i+1)+'.png'),(50,70)))
 def menu():
     global frame
+    global motion, switch
     running = True
     myClock = time.Clock()
+    rects=[(400,(y+5)*55) for y in range(4)]
     
+           
     while running:
         for evnt in event.get():          
             if evnt.type == QUIT:
@@ -21,8 +29,22 @@ def menu():
         
         screen.blit(menu_background,(0,0))
         screen.blit(door,(750,455))
+        screen.blit(glados,(55,0-motion))
         screen.blit(sprint[int(frame)%22],(400,500))
-        frame+=0.6
+        for pos in rects:
+            if (Rect(pos[0],pos[1],208,53)).collidepoint(mpos):
+                screen.blit(click_button,pos)
+            else:
+                screen.blit(button,pos)
+        frame+=0.7
+        if switch:
+            motion+=1
+            if motion==41:
+                switch=False
+        if switch==False:
+            motion-=1
+            if motion==-2:
+                switch=True
         
 
                 
