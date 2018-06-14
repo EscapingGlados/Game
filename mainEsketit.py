ca88fa108256476dde6d351717ea578cd851f097
adeef <feeda1539@gmail.com>
2018-06-14 08:43:49 -0400

    changes

--- mainEsketit.py
+++ mainEsketit.py
@@ -1,8 +1,17 @@
+#main game
 from pygame import *
-from math import *
-import time
 import os
 import pickle
+from math import *
+import time
+import glob
+##from MAIN import *
+##
+##from menu import *
+##
+##from LEVELEDIT import *
+
+screen=display.set_mode((800,600))
 
 def game():
 
@@ -63,7 +72,7 @@ def game():
     mode = 'idle'
 
     pl,pw=[50,60] #player length and width
-    px,py=[100,450] 
+    px,py=[100,450]
 
     hit = None
     hit1 = None         
@@ -131,10 +140,10 @@ def game():
                 return True
                 break
         return False
-    def move(playerpos,state,grav_velocity,oldpos,last_tp,forced_end):
+    def move(playerpos,state,grav_velocity,oldpos,last_tp,forced_end,mode,xchange,floatingmode,face,changing):
         '''Moves the player, including jumping. Also accounts for velocity gained from gravity.
     Also includes the moving of player concerning portals.'''
-        global mode,xchange,floatingmode,face,changing
+         
         playerpos=list(playerpos)
         startpos = playerpos[:]
         
@@ -275,7 +284,7 @@ def game():
             newpos=playerpos[:]
             playerpos=collide(oldpos,newpos,map_grid)
             
-        return playerpos,state,grav_velocity,oldpos,last_tp,forced_end
+        return playerpos,state,grav_velocity,oldpos,last_tp,forced_end,mode,xchange,floatingmode,face,changing
 
     def bullet_collide(pos):
         pos_rect=Rect(pos[0]-8,pos[1]-8,16,16)
@@ -413,7 +422,7 @@ def game():
 
     #----MOVING----------------------------------
         
-        (px,py),state,grav_velocity,oldpos,last_tp,forced_end=move([px,py],state,grav_velocity,oldpos,last_tp,forced_end)
+        (px,py),state,grav_velocity,oldpos,last_tp,forced_end,mode,xchange,floatingmode,face,changing=move([px,py],state,grav_velocity,oldpos,last_tp,forced_end,mode,xchange,floatingmode,face,changing)
         
 
     #----SHOOTING--------------------------------
@@ -455,6 +464,211 @@ def game():
         frame+=1
         oldpos=[px,py]
         display.flip()
-        
     quit()
+        
+def levelEd():
+
+
+    showing = True
+    typing = False
+    loadingScreen = False
+    font.init()
+    rekt = Rect(750,550,50,50)
+    loadRect = Rect(700,550,50,50)
+
+    def drawAll(screen,output,image):
+        screen.blit(image,(0,0))
+        for x in range(80):
+            for y in range(60):
+                c = level[x][y]
+                if c != 0:
+                    draw.rect(screen,col[c], (x*10,y*10,10,10))
+                    
+    def loadMap(fname):
+        if fname in os.listdir("."):
+            myPFile = open(fname, "rb")
+            return pickle.load(myPFile)       
+        else:
+            return [[0]*60 for x in range(80)]
+        
+    def saveMap(level, fname):
+        myPFile = open("Saves/"+fname, "wb")
+        pickle.dump(level, myPFile)
+
+    screen = display.set_mode((800,600))
+    col = [(0,0,0),(0,255,0),(255,255,255),(255,0,0),(0,0,255),(0,255,255),(175,119,22),(0,0,0)]#nothing,can portal,cant portal,jump pad, launch pad right, launch pad left 
+    current = 1
+    back = image.load("background.bmp")
+    level = loadMap("startXD.p")
+    colz = (255,0,0)
+    buttons = [[50,True],[120,False],[190,False],[260,False],[330,False],[400,False]]
+
+    upheav = font.Font("upheavtt.ttf",40)
+    name = ""
+    running = True
+
+    def text(msg,x,y):
+        words = upheav.render(msg, True,(0,255,255))
+        screen.blit(words,(x,y))
+    while running:
+        click = False
+        for e in event.get():                
+            if e.type == QUIT:
+                running = False
+
+            if e.type == MOUSEBUTTONDOWN:
+                if e.button == 1:
+                    click = True
+            if e.type == KEYDOWN:
+                keys = key.get_pressed()
+                if typing == True and e.key < 256:
+                    if keys[K_BACKSPACE]:
+                        name = name[:-1]
+                    elif keys[K_RETURN]:
+                        saveMap(level, str(name))
+                        typing = False
+                    else:
+                        name += e.unicode
+                        
+        mx, my = mouse.get_pos()
+        keys = key.get_pressed()                
+        for i in range(8):
+            if keys[i+48]:
+                if i < 1 or i > 6:
+                    pass
+                else:
+                    current = i
+                    
+        if click and rekt.collidepoint((mx,my)):
+            typing = True
+
+        if click and loadRect.collidepoint((mx,my)):
+            loadingScreen = True
+            picList = glob.glob("Saves/*")
             
+                
+            print(picList)
+
+        if keys[K_ESCAPE] and showing == True:
+            showing = False
+
+        
+        elif keys[K_ESCAPE] and showing == False:
+            showing = True
+            
+        if mouse.get_pressed()[0]:
+            gx = mx // 10
+            gy = my // 10
+            level[gx][gy] = current
+            draw.rect(screen, col[level[gx][gy]], (gx*10, gy*10, 10, 10))
+            
+            
+        if mouse.get_pressed()[2]:
+            mx, my = mouse.get_pos()
+            gx = mx // 10
+            gy = my // 10
+            level[gx][gy] = 0
+            
+        drawAll(screen, level, back)
+        
+        if typing == True:
+            draw.rect(screen,(0,0,0),(0,0,600,50))
+            text(name,0,0)
+            
+        buttons[current-1][1] = True
+        
+        for i in range(6):
+            if buttons[i] != buttons[current-1]:
+                buttons[i][1] = False
+            if buttons[i][1] == False:
+                colz = (255,0,0)
+                
+            if buttons[i][1] == True:
+                colz = (0,255,0)
+            if showing == True:
+                draw.rect(screen,(colz),(buttons[i][0],50,50,50),3)
+        draw.rect(screen,(0,0,255),rekt)
+        draw.rect(screen,(255,0,0),loadRect)
+                    
+        if loadingScreen == True:
+            for i in range(len(picList)):
+                if Rect(200,50*i+200,600,50).collidepoint((mx,my)):
+                    if click:
+                        print("noggers")
+                        level = loadMap(picList[i][6:]+".p")
+                    draw.rect(screen,(255,0,0),(200,50*i+200,600,50))
+                
+                text(picList[i][6:],200,50*i+200)
+                    
+        
+        display.flip()
+
+    quit()
+
+def starting():
+    global gameType
+    menu_background=image.load('menu_background.png')
+    door=image.load('door.png')
+    glados=transform.scale(image.load('glados.png'),(248,362))
+    sprint=[]
+    button=image.load('button.png')
+    click_button=image.load('button_on_hover.png')
+    frame=0
+    motion=0
+    switch=True
+    for i in range(2,26):
+        sprint.append(transform.smoothscale(image.load(str(i+1)+'.png'),(50,70)))
+        
+    running = True
+    rects=[(400,(y+5)*55) for y in range(4)]
+    print(rects)
+    texts=[]
+    for i in range(4):
+        texts.append(image.load('B%s.png'%(i+1)))
+
+           
+    while running:
+        click = False
+        for e in event.get():          
+            if e.type == QUIT:
+                running = False
+            if e.type == MOUSEBUTTONDOWN:
+                if e.button == 1:
+                    click = True
+
+        mpos = mouse.get_pos()
+        mb = mouse.get_pressed()
+        
+        screen.blit(menu_background,(0,0))
+        screen.blit(door,(750,455))
+        screen.blit(glados,(55,0-motion))
+        screen.blit(sprint[int(frame)%22],(400,500))
+        for pos in range(len(rects)):
+            if (Rect(rects[pos][0],rects[pos][1],208,53)).collidepoint(mpos):
+                screen.blit(click_button,rects[pos])
+            else:
+                screen.blit(button,rects[pos])
+            screen.blit(texts[pos],rects[pos])
+        frame+=0.7
+        if switch:
+            motion+=1
+            if motion==41:
+                switch=False
+        if switch==False:
+            motion-=1
+            if motion==-2:
+                switch=True
+        
+        if (Rect(rects[0][0],rects[0][1],208,53)).collidepoint(mpos) and click:
+            running = False
+            game()
+            break
+        if (Rect(rects[1][0],rects[1][1],208,53)).collidepoint(mpos) and click:
+            running = False
+            levelEd()
+            break
+    
+        display.flip()
+    quit()
+
+starting()
--- menu.py
+++ menu.py
@@ -1,26 +1,28 @@
 from pygame import *
-screen=display.set_mode((800,600))
-menu_background=image.load('menu_background.png')
-door=image.load('door.png')
-glados=transform.scale(image.load('glados.png'),(248,362))
-sprint=[]
-button=image.load('button.png')
-click_button=image.load('button_on_hover.png')
-frame=0
-motion=0
-switch=True
-for i in range(2,26):
-    sprint.append(transform.smoothscale(image.load(str(i+1)+'.png'),(50,70)))
-def menu():
-    global frame
-    global motion, switch
+import os
+import pickle
+from pprint import *
+def starting():
+
+    menu_background=image.load('menu_background.png')
+    door=image.load('door.png')
+    glados=transform.scale(image.load('glados.png'),(248,362))
+    sprint=[]
+    button=image.load('button.png')
+    click_button=image.load('button_on_hover.png')
+    frame=0
+    motion=0
+    switch=True
+    for i in range(2,26):
+        sprint.append(transform.smoothscale(image.load(str(i+1)+'.png'),(50,70)))
+        
     running = True
     myClock = time.Clock()
     rects=[(400,(y+5)*55) for y in range(4)]
     texts=[]
     for i in range(4):
         texts.append(image.load('B%s.png'%(i+1)))
-    
+
            
     while running:
         for evnt in event.get():          
@@ -55,4 +57,4 @@ def menu():
         display.flip()
     quit()
     
-menu()
+
