71542e60a1047d37fa30635d94cf0b6f703e58d9
adeef <feeda1539@gmail.com>
2018-06-15 08:47:16 -0400

    stuff

--- menu.py
+++ menu.py
@@ -10,6 +10,7 @@ def Main():
     menu_background=image.load('menu_background.png')
     door=image.load('door.png')
     glados=transform.scale(image.load('glados.png'),(248,362))
+    tutorial = image.load("How-to.png")
     sprint=[]
     button=image.load('button.png')
     click_button=image.load('button_on_hover.png')
@@ -21,14 +22,14 @@ def Main():
 
     running = True
     myClock = time.Clock()
-    rects=[(400,(y+5)*55) for y in range(4)]#first rects diplayes (play,create,options)
+    rects=[(400,(y+5)*55) for y in range(3)]#first rects diplayes (play,create,options)
     rects2 =[(400,(y+5)*55) for y in range(3)]
     texts=[]
     texts2 = []
     coloured = []
     
 
-    for i in range(4):
+    for i in range(3):
         texts.append(image.load('B%s.png'%(i+1)))
     for i in range(3):
         texts2.append(image.load('A%s.png'%(i+1)))
@@ -54,6 +55,8 @@ def Main():
             running = False
         elif click and Rect(rects[1][0],rects[1][1],208,53).collidepoint(mpos) and blitMode == 'first':
             blitMode = 'edit'
+        elif click and Rect(rects[2][0],rects[2][1],208,53).collidepoint(mpos):
+            blitMode = 'tut'
         
         elif click and Rect(rects2[0][0],rects2[0][1],240,50).collidepoint(mpos) and blitMode == 'edit':
             operation = 'edit'
@@ -72,7 +75,7 @@ def Main():
         screen.blit(sprint[int(frame)%22],(400,500))
         
  #      print(blitMode)
-        if blitMode == 'first':
+        if blitMode == 'first':#first screen you see
             for pos in range(len(rects)):
                 if (Rect(rects[pos][0],rects[pos][1],208,53)).collidepoint(mpos):
                     screen.blit(click_button,rects[pos])
@@ -80,14 +83,16 @@ def Main():
                     screen.blit(button,rects[pos])
                 screen.blit(texts[pos],rects[pos])
                 
-        elif blitMode == 'edit':
+        elif blitMode == 'edit':#create your own level screen
             for pos in range(len(rects2)):
                 if (Rect(rects2[pos][0],rects2[pos][1],240,50)).collidepoint(mpos):
                     screen.blit(coloured[pos],rects2[pos])
                 else:
                     screen.blit(texts2[pos],rects2[pos])
  #               screen.blit(texts2[pos],rects2[pos]) 
-            
+        elif blitMode == 'tut':#how-to screen
+            screen.blit(tutorial,(0,0))
+#            print("yeet")
         frame+=0.7
         if switch:
             motion+=1
